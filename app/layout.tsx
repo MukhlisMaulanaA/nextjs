84ddb54bd38afafca2aed20dc6e47185ba2308a7
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-01-01 22:56:15 +0700

    firstcom

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import ScrollToTop from '@/components/ScrollToTop';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import '../node_modules/react-modal-video/css/modal-video.css';
+
+// import './globals.css';
+import "../styles/index.css";
+import { Providers } from './providers';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="dark:bg-black">
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+          <ScrollToTop />
+        </Providers>
+      </body>
+    </html>
+  )
+}
