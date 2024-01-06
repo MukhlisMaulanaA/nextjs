fe087220c5a781336c5f1df93f99e6c8ba7a0be8
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-01-06 18:11:59 +0700

    update: add web analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,13 @@ import ScrollToTop from '@/components/ScrollToTop';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import '../node_modules/react-modal-video/css/modal-video.css';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 // import './globals.css';
 import "../styles/index.css";
 import { Providers } from './providers';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function RootLayout({
   children,
@@ -19,6 +21,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className="dark:bg-black">
         <SpeedInsights/>
+        <Analytics />
         <Providers>
           <Header />
           {children}
