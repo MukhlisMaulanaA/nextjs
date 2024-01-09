03430f4eec7ab770e96b490727e9d6280834c67f
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-01-09 17:22:19 +0700

    fix: gallery layouts and edit photo team

--- components/Gallery/SingleGallery.tsx
+++ components/Gallery/SingleGallery.tsx
@@ -6,8 +6,8 @@ const SingleGallery = ({ gallery }: { gallery: Gallery }) => {
   const { title, image, date } = gallery;
   return (
     <>
-      <div className="wow fadeInUp relative overflow-hidden rounded-md bg-white shadow-one dark:bg-dark" data-wow-delay=".1s">
-        <Image src={image} alt="gallery1" fill/>
+      <div className="wow fadeInUp flex relative overflow-hidden rounded-md bg-white shadow-one dark:bg-dark" data-wow-delay=".1s">
+        <Image src={image} alt="gallery1" fill style={{ objectFit: "cover" }}/>
         <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-70 duration-300">
           <h1 className="title-font text-dark text-2xl font-medium text-gray-900 mb-3">{title}</h1>
           <a href="" className="text-dark">{date}</a>
